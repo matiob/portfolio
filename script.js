fd8d185e53c552ba8df8a7a5d4a41a44b293397d
matiob <buraschi.mateo@gmail.com>
2024-02-08 16:45:41 -0300

    portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    const icons = document.querySelector('.hamburguer-icon');
+    menu.classList.toggle('open')
+    icons.classList.toggle('open')
+}
